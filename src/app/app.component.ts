bcde21a25531dc41319206e9f159c24cf784293d
Natan Fagundes <natan.fags@gmail.com>
2018-12-13 16:59:01 -0200

    implementado variaveis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  numeroUm: number = 2;
+  numeroDois: number = 10;
+  operacao: string = 'SOMA';
+
   title = 'calculadora-frontend';
 }
